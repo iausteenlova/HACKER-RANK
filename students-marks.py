3595c92bced4c5cc8fb4b5889866ea905b31bc94
John Austeen <shajohn30@gmail.com>
2019-09-18 18:24:12 +0530

    Create students-marks.py
    Given the names and grades for each student in a Physics class of  N students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.

---
+++ students-marks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+marksheet=[]
+scorelist=[]
+if __name__ == '__main__':
+        for _ in range(int(input("Enter no. of students"))):
+                name = input("name :")
+                score = float(input("marks :"))
+                marksheet+=[[name,score]]
+                scorelist+=[score]
+        b=sorted(list(set(scorelist)))[1]
+        
+
+        for name,score in sorted(marksheet):
+             if score==b:
+                    print(f"The second highest scorer is {name}. and the score is {b}.")
+
+
+# In[ ]:
+
+
+
+
